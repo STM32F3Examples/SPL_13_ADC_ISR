875c7e7f7bec2f610aa83708b269d92e9f66b447
Manuel Limones <limonesu.me@gmail.com>
2015-11-17 09:32:49 -0600

    use adc with timer15

--- source/main.c
+++ source/main.c
@@ -3,46 +3,10 @@
 #include "ctimers_stm32f3.h"
 
 int main(){
-	timer2_init(100,PERIOD_IN_MICROSECONDS);
-	adc_injected(ADC_TRIGGER_TIMER2);
-	timer2_start();
-
-	timer15_init(1000,PERIOD_IN_MICROSECONDS);
-	timer15_enableIRQ();
+	timer15_init(100,PERIOD_IN_MICROSECONDS);
+	adc_injected(ADC_TRIGGER_TIMER15);
 	timer15_start();
-	
-	timer16_init(2000,PERIOD_IN_MILLISECONDS);
-	timer16_enableIRQ();
-	timer16_start();
-	
-	timer17_init(4000,PERIOD_IN_MICROSECONDS);
-	timer17_enableIRQ();
-	timer17_start();
-	
-	timer6_init(8000,PERIOD_IN_MICROSECONDS);
-	timer6_enableIRQ();
-	timer6_start();
 	
 	while(1){
 	}
-}
-
-int timer15_counter = 0;
-void timer15_callback(void){
-	timer15_counter++;
-}
-
-int timer16_counter = 0;
-void timer16_callback(void){
-	timer16_counter++;
-}
-
-int timer17_counter = 0;
-void timer17_callback(void){
-	timer17_counter++;
-}
-
-int timer6_counter = 0;
-void timer6_callback(void){
-	timer6_counter++;
 }
